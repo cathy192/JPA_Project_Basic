d4212bb31c4b554924c5f6dbe2f8a3f18ddb8d9e
cathy192 <korea@2047>
2020-12-14 15:41:43 +0900

    상품 리퍼지터리 개발

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.Domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em ;
+
+    public void save(Item item){
+        if(item.getId() ==null)//새로 생상한 객체이다
+             em.persist(item); //신규 등록
+        else
+            em.merge(item); //이미 등록된걸 update와 비슷
+
+    }
+
+    public Item findOne(Long id){
+        return em.find(Item.class,id);
+    }
+    public List<Item> findAll(Long id){
+        return em.createQuery("select i from Item i", Item.class)
+        .getResultList();
+    }
+}
